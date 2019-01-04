bcc6c3912d349159f0037b22d8c3cdedd5d32df4
Benjamin Simon <git.besim@gmail.com>
2019-01-04 17:27:30 +0100

    It works now.

--- heuristic.c
+++ heuristic.c
@@ -1,19 +1,19 @@
 #include "base.h"
 #include "definitions.h"
 
-#define occupied(state) ((state->board[0] | state->board[1]))
-#define empty(state) ~(occupied(state))
-#define shift_xy(x, y) (x + BOARD_WIDTH * y)
-#define field_at(x, y) (ONE << shift_xy(x, y))
-#define is_set(b, x, y) b &field_at(x, y)
+// #define occupied(state) ((state->board[0] | state->board[1]))
+// #define empty(state) ~(occupied(state))
+// #define shift_xy(x, y) (x + BOARD_WIDTH * y)
+// #define field_at(x, y) (ONE << shift_xy(x, y))
+// #define is_set(b, x, y) b &field_at(x, y)
 
-#define CORNERS 0x8100000000000081
-#define C_SPOTS 0x4281000000008142
-#define X_SPOTS 0x42000000004200
-#define ONE_SPOTS 0x182424180000
-#define TWO_SPOTS 0x240000240000
-#define FOUR_SPOTS 0x3C424242423C00
-#define SIX_SPOTS 0x3C0081818181003C
+// #define CORNERS 0x8100000000000081
+// #define C_SPOTS 0x4281000000008142
+// #define X_SPOTS 0x42000000004200
+// #define ONE_SPOTS 0x182424180000
+// #define TWO_SPOTS 0x240000240000
+// #define FOUR_SPOTS 0x3C424242423C00
+// #define SIX_SPOTS 0x3C0081818181003C
 
 #define POSITION_STACK_SIZE 64
 typedef struct {
@@ -68,21 +68,25 @@ static inline int heuristic(uint_fast64_t pos)
 uint_fast64_t random_move(uint_fast64_t possible) {
   int count = rand() % (64 - __builtin_clzll(possible));
   printf("%llx >> %i = %llx\n", possible, count, possible >> count);
-  count += __builtin_ctzll(possible>>count);
+  count += ctzll(possible>>count);
   return ONE << count & possible;
 }
 
 uint_fast64_t most_promising_move(uint_fast64_t possible)
 {
-  int count = 1; //where y = count / height +1 and x = count % width + 1;
+  int count = 0; //where y = count / height +1 and x = count % width + 1;
   uint_fast64_t best_move = 0;
   int best_score = 0;
 
   while (possible)
   {
     printf("%llx\n", possible);
-    count += __builtin_ctzll(possible);
-    possible >>= __builtin_ctzll(possible)+1;
+    count += ctzll(possible);
+    possible >>= ctzll(possible) + 1;
+
+    // printf("Current move: %c%i\n", 
+    //       (ctzll(ONE << count) % BOARD_WIDTH + 'A'),
+    //       (ctzll(ONE << count) / BOARD_HEIGHT) + 1);
 
     // if (possible & 1)
     int current_score = heuristic(ONE << count);
@@ -96,6 +100,7 @@ uint_fast64_t most_promising_move(uint_fast64_t possible)
     }
     // best_move = current_score > best_score ? ONE << count : best_move;
     // best_score = current_score > best_score ? ONE << current_score : best_score;
+    count++;
   }
 
   return random_move(best_move);
@@ -108,7 +113,8 @@ int main(int argc, char const *argv[])
   uint_fast64_t most_promising = most_promising_move(test);
   // uint_fast64_t most_promising = ONE;
   printf("Most promising move: %c%i\n", 
-          (__builtin_ctzll(most_promising) % BOARD_WIDTH) + 'A',
-          (__builtin_ctzll(most_promising) / BOARD_HEIGHT)+1);
+          (ctzll(most_promising) % BOARD_WIDTH) + 'A',
+          (ctzll(most_promising) / BOARD_HEIGHT)+1);
+  printf("%i\n", popcount(0xE));
   return 0;
 }
